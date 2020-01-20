1cc68674256ed9d564f4c52edca8db33f8c24c5e
mitchneides <mitch.neides@gmail.com>
2020-01-20 18:52:58 +0200

    finishes last variable exercise

--- Week_2/day_2/exercises/xp_variables/script.js
+++ Week_2/day_2/exercises/xp_variables/script.js
@@ -9,28 +9,53 @@
 
 // Exercise 2 and 3
 
-var pets = ['cat', 'dog', 'fish', 'rabbit', 'cow'];
-console.log(pets[1]);
-pets.push('horse');
-console.log(pets);
-pets.splice(3,1);
-console.log(pets);
-console.log("The length of the array is " +pets.length +".");
+// var pets = ['cat', 'dog', 'fish', 'rabbit', 'cow'];
+// console.log(pets[1]);
+// pets.push('horse');
+// console.log(pets);
+// pets.splice(3,1);
+// console.log(pets);
+// console.log("The length of the array is " +pets.length +".");
 
 
-var newDog = "Chihuahua";
-console.log("There are " +newDog.length +" letters in " +newDog);
-console.log(newDog.toUpperCase());
-console.log(newDog.toLowerCase());
+// var newDog = "Chihuahua";
+// console.log("There are " +newDog.length +" letters in " +newDog);
+// console.log(newDog.toUpperCase());
+// console.log(newDog.toLowerCase());
 
-if (newDog == "Chihuahua") {
-	alert("I LOVE Chihuahua, it's my favorite " +pets[1]);
-}
-else {
-	console.log("I dont care, I prefer CATS");
-}
+// if (newDog == "Chihuahua") {
+// 	alert("I LOVE Chihuahua, it's my favorite " +pets[1]);
+// }
+// else {
+// 	console.log("I dont care, I prefer CATS");
+// }
 
 
 // Exercise 4
 
+// var birthYear = 1994;
+// var age;
 
+// var someYear = prompt("Enter a year: ")
+// getAge(someYear);
+
+// function getAge (year) {
+// 	age = year-birthYear;
+// 	console.log("I will be " +age +" in " +year);
+// 	return age;
+// }
+
+
+// Exercise 5
+
+var firstName = "Mitch";
+var lastName = "Neides";
+var fullName = firstName+lastName;
+
+var a = 34; var b = 21; a = 2; 
+
+a + b //equals 23
+
+var c;
+
+//c is undefined
