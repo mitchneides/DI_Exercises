0c9d1f6fcbdfa20175046203ce28806b9fa10856
mitchneides <mitch.neides@gmail.com>
2020-02-03 14:54:24 +0200

    adds class notes w4d2

---
+++ Week_4/Day_2/class/class_notes.py
@@ -0,0 +1,14 @@
+selection_menu = None
+
+menu_text = "Welcome to the menu.\nPlease select what you would like to do:\na. print hello\nb. say your name\nx. exit\nYour choice: "
+
+while selection_menu != 'x':
+    selection_menu = input(menu_text)
+    if selection_menu == 'a':
+        print('\nHello!\n')
+    elif selection_menu == 'b':
+        user_name = input("Tell me your name: ")
+        print(f"\nHello {user_name}!\n")
+
+if selection_menu == 'x':
+    print("Goodbye")
