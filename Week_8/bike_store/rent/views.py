2c377260dde52bd490b52de695e84f34d2388001
mitchneides <mitch.neides@gmail.com>
2020-03-07 21:23:49 +0200

    updates queries for rental page

--- Week_8/bike_store/rent/views.py
+++ Week_8/bike_store/rent/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rent.models import Rental, Customer, Vehicle
+from rent.models import Rental, Customer, Vehicle, VehicleType
 
 
 def index(request):
@@ -16,10 +16,21 @@ def vehicle(request):
 
 def rental(request):
     rental_data = Rental.objects.all().order_by('-return_date')
-    
-    customers = Customer.objects.all()
-    vehicles = Vehicle.objects.all()
-# need to get the type of the vehicle, not just the object ////////////////////////////
-    # party = Party.objects.get(pk=pk)
 
-    return render(request, template_name='rent/rental.html', context={'rentals': rental_data, 'customers': customers, 'vehicles': vehicles})
+    vehicle_ids = []
+    customer_ids = []
+    for c,item in enumerate(rental_data):
+        vehicle_ids.append(Rental.objects.get(pk=c+1).vehicle_id)
+        customer_ids.append(Rental.objects.get(pk=c+1).customer_id)
+
+    vehicle_names = []
+    for item in vehicle_ids:
+        print(item)
+        vehicle_names.append(VehicleType.objects.get(pk=item).type)
+
+    customer_names = []
+    for item in customer_ids:
+        customer_names.append(Customer.objects.get(pk=item).name)
+
+
+    return render(request, template_name='rent/rental.html', context={'rentals': rental_data, 'customers': customer_names, 'vehicles': vehicle_names})
